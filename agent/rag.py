cd4afcd610c5805404b91431eed4c5f45842e70f
shestakovden <shestakovden@yandex-team.ru>
2024-12-23 22:57:58 +0600

    Add rerank

--- agent/rag.py
+++ agent/rag.py
@@ -1,6 +1,10 @@
 import requests
+from transformers import CrossEncoder
 
 rag_url = "http://213.139.208.158:8000/search"
+
+model_name = "cross-encoder/ms-marco-T5-12-v1"
+model = CrossEncoder(model_name)
 
 def retrieve_related_chunks(query: str) -> dict:
     url = rag_url
@@ -11,10 +15,13 @@ def retrieve_related_chunks(query: str) -> dict:
     }
     response = requests.get(url, params=params)
     if response.status_code == 200:
-        chunks = response.json()[:3]
+        chunks = response.json()
+        inputs = [[query, result] for result in chunks]
+        scores = model.predict(inputs)
+        reranked_chunks = [result for _, result in sorted(zip(scores, chunks), reverse=True)]
         print(len(chunks))
         print(chunks)
-        return {"chunks": chunks}
+        return {"chunks": reranked_chunks[:1]}
     else:
         return {"error": f"API request failed with status code {response.status_code}"}
 
